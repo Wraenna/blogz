7c95906318559203a54dabf825484b8a7525b1b4
Wren Bareilles <wren.ellbee@gmail.com>
2017-07-10 17:56:24 -0700

    Created Post class. Set up blog and newpost routes.

--- main.py
+++ main.py
@@ -7,25 +7,43 @@ app.config['DEBUG'] = True
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:getitdone@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 app.secret_key = 'y337kGcys&zP3B'
 
-class User(db.Model):
-    # TODO - write user class
-
 class Post(db.Model):
-    # TODO - write post class
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 # TODO - add the following routes:
-# "/" for the index - would be good to make a register or login page of it
-# "/blog" which displays all the blog posts
-# "/newpost" which allows the user to add a new posts
+# "/newpost" which allows the user to add a new posts - should redirect to "/"
 
-# TODO - add the following templates:
-# "register.html" to register
-# "login.html" to login
-# "blog.html" after logged in to display all posts
-# "newpost.html" to add a new post
+@app.route("/blog", methods=['POST', 'GET'])
+def index():
+
+    posts = Post.query.all()
+
+    return render_template("blog.html", title="My Blog", posts=posts)
+
+@app.route("/newpost", methods=['POST', 'GET'])
+def new_post():
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_body = request.form['body']
+        new_post = Post(post_title, post_body)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect("/blog")
+
+    return render_template("newpost.html", title="New Post")
+
+if __name__ == '__main__':
+    app.run()
