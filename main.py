43661a9dd2503a7736cb6162ab8f8c2e3ce47ff1
Wren Bareilles <wren.ellbee@gmail.com>
2017-07-11 20:23:33 -0700

    Completed assignment objectives.

--- main.py
+++ main.py
@@ -22,26 +22,52 @@ class Post(db.Model):
         self.title = title
         self.body = body
 
-# TODO - add the following routes:
-# "/newpost" which allows the user to add a new posts - should redirect to "/"
 
-@app.route("/blog", methods=['POST', 'GET'])
+@app.route("/")
 def index():
+    return redirect("/blog")
 
+# Main route. This route renders the blog with all the entries.
+@app.route("/blog", methods=['POST', 'GET'])
+def blog():
+    # First check to see if there are any query parameters
+    blog_post_id = request.args.get('id')
+    # If it finds a query parameter, it renders only the post that matches the id
+    if blog_post_id:
+        posts = Post.query.filter_by(id=blog_post_id).all()
+        return render_template("blog.html", title="My Blog", posts=posts)
+    # Query for all the posts.
     posts = Post.query.all()
-
+    # Render the template
     return render_template("blog.html", title="My Blog", posts=posts)
 
+# Newpost route. This route allows you to add a new post.
 @app.route("/newpost", methods=['POST', 'GET'])
 def new_post():
 
     if request.method == 'POST':
-        post_title = request.form['title']
-        post_body = request.form['body']
+        post_title = request.form['post_title']
+        post_body = request.form['post_body']
+        # TODO - Add error messages if title or body is blank
+        # Have these flash as appropriate
+        if post_title == "" or post_body == "":
+            if post_title == "":
+                flash("Please enter a title.", "error")
+
+            if post_body == "":
+                flash("Please enter a body.", "error")
+
+            return render_template("newpost.html", title="New Post", post_title=post_title, post_body=post_body)
+        # If there are no errors, go ahead and make the post
         new_post = Post(post_title, post_body)
         db.session.add(new_post)
         db.session.commit()
-        return redirect("/blog")
+
+        # TODO - Once the commit is done, query the new post
+        # Maybe do this by the post title? Or post body?
+        # The odds of it being duplicate are low
+
+        return redirect("/blog?id=" + str(new_post.id))
 
     return render_template("newpost.html", title="New Post")
 
