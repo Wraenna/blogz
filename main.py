2ea424b722fdc4f35faf5f0cdd8b951191eaedd3
Wren Bareilles <wren.ellbee@gmail.com>
2017-07-13 13:52:32 -0700

    Blog posts are now sorted newest to oldest before rendered.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -18,10 +19,14 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
+    pub_date = db.Column(db.DateTime)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, pub_date=None):
         self.title = title
         self.body = body
+        if pub_date is None:
+            pub_date = datetime.utcnow()
+        self.pub_date = pub_date
 
 
 # Index route redirects to /blog
@@ -44,7 +49,7 @@ def blog():
         return render_template("blog.html", title=posts[0].title, posts=posts)
 
     # Query for all the posts.
-    posts = Post.query.all()
+    posts = Post.query.order_by(Post.pub_date.desc()).all()
 
     # Render the template
     return render_template("blog.html", title="My Blog", posts=posts)
