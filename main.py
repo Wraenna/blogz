f9e254aacd4c6c85b36437164dee7deaf9941699
Wren Bareilles <wren.ellbee@gmail.com>
2017-07-24 15:57:55 -0700

    Added static to allowed routes for CSS management.

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup', 'index', 'blog']
+    allowed_routes = ['login', 'signup', 'index', 'blog', 'static']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
